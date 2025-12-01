4355bcf6879ce3635670234d685e1949b7265c51
Naman Karki <namankarki.nbk@gmail.com>
2025-12-01 19:12:01 +0545

    decorators

--- oop/decorators.py
+++ oop/decorators.py
@@ -20,4 +20,41 @@
 # slow_function()
 
 
-#decorator with parameters
+#staticmethod is a decorator that is used to define a method that does not operate on an instance of the class
+#classmethod is a decorator that is used to define a method that operates on the class itself
+#abstractmethod is a decorator that is used to define a method that must be implemented by any subclass
+
+
+# import abc
+
+# class Animal(abc.ABC):
+#     @abc.abstractmethod
+#     def sound(self):
+#         pass    
+
+# class Dog(Animal):
+#     def sound(self):
+#         print("Bark")
+
+# dog = Dog()
+# dog.sound()
+
+
+
+from abc import ABC, abstractmethod
+
+class teacher(ABC):
+
+    @abstractmethod
+    def teaches(self):
+        pass
+
+
+class MathTeacher(teacher):
+    def teaches(self):
+        print("Math Teacher")
+
+
+
+obj1 = MathTeacher()
+obj1.teaches()
